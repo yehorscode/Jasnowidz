8c3508e4ba4b1d3d7f96c985280558062e94c2b6
Yehor <omelianenkoyehor@proton.me>
2026-08-22 14:20:22 +0200

    [ai] added url for @ and vercel added itself

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -3,9 +3,7 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
 
@@ -13,7 +11,12 @@ export default defineConfig({
   integrations: [react()],
 
   vite: {
-      plugins: [tailwindcss()],
+		plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": new URL("./src", import.meta.url).pathname,
+      },
+    },
 	},
 
   adapter: vercel(),
