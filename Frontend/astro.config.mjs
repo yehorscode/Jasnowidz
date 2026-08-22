8483ed8ea4bfbf858b8a248c7c8f992b07489c18
Yehor <omelianenkoyehor@proton.me>
2026-08-22 14:14:51 +0200

    add vercel

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -4,12 +4,17 @@ import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	// Enable React to support React JSX components.
-	integrations: [react()],
-	vite: {
-		plugins: [tailwindcss()],
+
+  // Enable React to support React JSX components.
+  integrations: [react()],
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: vercel(),
 });
